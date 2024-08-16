3b42257876dee77759275bdb0f496e4e93a16fe9
Diana Cristea <dicristea111@gmail.com>
2024-08-15 17:22:27 -0700

    background veggies

--- src/components/routes/Home.jsx
+++ src/components/routes/Home.jsx
@@ -17,15 +17,42 @@ import DinnerArtwork from '../../assets/artwork/Homepage_dinner.svg';
 import ChefArtwork from '../../assets/artwork/Homepage_Block.svg';
 import HomeAnimation from '../../assets/HomeFeedAnimation.gif';
 import DownloadSection from '../general/DownloadSection';
+import Mushroom from '../../assets/artwork/mushroom.png';
+import Pepper from '../../assets/artwork/pepper.png';
+import Broccoli from '../../assets/artwork/broccoli.png';
+import useScreenSize from '../../utils/useScreenSize';
+
 // import ActionButton from '../general/ActionButton';
 
 export default function Home() {
   const signUpRef = useRef();
+  const screenSize = useScreenSize();
 
   return (
     <div>
       <Header className={fontColorWhite} page="Title" signUpRef={signUpRef} />
       <HomeSection>
+        <img
+          alt="mushroom artwork"
+          className={mushroom}
+          height={screenSize.width > 850 ? '300px' : '200px'}
+          src={Mushroom}
+          width={screenSize.width > 850 ? '300px' : '200px'}
+        />
+        <img
+          alt="pepper artwork"
+          className={pepper}
+          height={screenSize.width > 800 ? '300px' : '200px'}
+          src={Pepper}
+          width={screenSize.width > 800 ? '300px' : '200px'}
+        />
+        <img
+          alt="broccoli artwork"
+          className={broccoli}
+          height={screenSize.width > 800 ? '300px' : '200px'}
+          src={Broccoli}
+          width={screenSize.width > 800 ? '300px' : '200px'}
+        />
         <div className={leftAlign}>
           <Heading3 className={h3PaddingBottom}>Where your next dinner plans are found.</Heading3>
           <BodyText>Date night? Parents coming into town?</BodyText>
@@ -87,6 +114,10 @@ export const HomeSection = styled.div`
   justify-content: center;
   justify-items: center;
   gap: 4rem;
+  position: relative;
+  div {
+    z-index: 100;
+  }
   @media (max-width: 800px) {
     gap: 2.5rem;
   }
@@ -95,6 +126,33 @@ export const HomeSection = styled.div`
     grid-template-columns: 1fr;
     grid-template-rows: 1fr;
   }
+`;
+
+const mushroom = css`
+  position: absolute;
+  z-index: 2;
+  opacity: 0.33;
+  top: 15px;
+  left: 10px;
+  transform: rotate(-20deg);
+`;
+
+const pepper = css`
+  position: absolute;
+  z-index: 2;
+  opacity: 0.33;
+  top: 300px;
+  right: -20px;
+  /* transform: rotate(-25deg); */
+`;
+
+const broccoli = css`
+  position: absolute;
+  z-index: 2;
+  opacity: 0.33;
+  top: 650px;
+  left: -10px;
+  transform: rotate(30deg);
 `;
 
 const item4 = css`
